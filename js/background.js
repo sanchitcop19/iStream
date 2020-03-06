d91b78f06c966354134337bb35944fdb568fd2b5
Sanchit <sanchitcop19@Gmail.com>
2020-03-06 18:37:21 -0500

    Hulu integration at alpha stage

--- js/background.js
+++ js/background.js
@@ -6,51 +6,54 @@ var SCRIPTS_TO_LOAD = ["libs/jquery-3.1.1.min.js", "js/content.js"];
 
 // Load the specified file and replace all placeholders before injecting it to the client side
 var getCodeForInjection = function(filename, replacements) {
-	var code = readFileFromCache(filename);
-	// Replace all placeholders
-	for (var key in replacements) {
-		if (replacements.hasOwnProperty(key)) {
-			var val = replacements[key];
-			// Encode \ to \\, ' to \', trim all unnecessary white space and line breaks
-			val = val.replace(/\\/g, "\\\\").replace(/'/g, "\\'").replace(/[\t\n\r]+/g, ' ');
-			// Replace @@PLACEHOLDER@@ with value
-			code = code.replace('@@' + key + '@@', val);
-		}
-	}
-	return code;
+    var code = readFileFromCache(filename);
+    // Replace all placeholders
+    for (var key in replacements) {
+        if (replacements.hasOwnProperty(key)) {
+            var val = replacements[key];
+            // Encode \ to \\, ' to \', trim all unnecessary white space and line breaks
+            val = val
+                .replace(/\\/g, "\\\\")
+                .replace(/'/g, "\\'")
+                .replace(/[\t\n\r]+/g, " ");
+            // Replace @@PLACEHOLDER@@ with value
+            code = code.replace("@@" + key + "@@", val);
+        }
+    }
+    return code;
 };
 
 // Inject page.js to the tab to initialize the extension
 var injectPageJsToTab = function(tab) {
-	log("Injecting loader script to tab " + tab.id);
+    log("Injecting loader script to tab " + tab.id);
 
-	// Load all scripts
-	var code = "";
-	for (var i in SCRIPTS_TO_LOAD) {
-		var script = SCRIPTS_TO_LOAD[i];
-		code += getCodeForInjection(script, {}) + ";\n\n";
-	}
+    // Load all scripts
+    var code = "";
+    for (var i in SCRIPTS_TO_LOAD) {
+        var script = SCRIPTS_TO_LOAD[i];
+        code += getCodeForInjection(script, {}) + ";\n\n";
+    }
 
-	// Inject it to page
-	chrome.tabs.executeScript(tab.id, {
-		code: code,
-		runAt: 'document_start',
-		allFrames: false // not to iframes inside
-	});
+    // Inject it to page
+    chrome.tabs.executeScript(tab.id, {
+        code: code,
+        runAt: "document_start",
+        allFrames: false // not to iframes inside
+    });
 };
 
 // Initialize the extension on the specified tab
 var initializeTab = function(tab, startUpRetroactiveLoad) {
-	// Inject page.js to the tab
-	injectPageJsToTab(tab);
+    // Inject page.js to the tab
+    injectPageJsToTab(tab);
 };
 
 // Loads all assets (JS/CSS/HTML) from disk to cache
 var loadAllAssetsToCache = function(debug, callback) {
-	// Load all files from disk to cache
-	readFilesFromDisk(debug, SCRIPTS_TO_LOAD, function() {
-		callback();
-	});
+    // Load all files from disk to cache
+    readFilesFromDisk(debug, SCRIPTS_TO_LOAD, function() {
+        callback();
+    });
 };
 //----------------------------------------------------------------------------------------------------------------------------------------------------
 // Initialization Helpers
@@ -58,54 +61,55 @@ var loadAllAssetsToCache = function(debug, callback) {
 
 // Called every time a tab is updated
 var onTabUpdate = function(tabId, info, tab) {
-	if (info.status == 'loading') {
-		log("=========================");
-		log("Tab updated: " + getTabName(tab) + ", status: ", info);
-		initializeTab(tab, false);
-	}
+    if (info.status == "loading") {
+        log("=========================");
+        log("Tab updated: " + getTabName(tab) + ", status: ", info);
+        initializeTab(tab, false);
+    }
 };
 
 // Called every time a tab is replaced
 var onTabReplace = function(addedTabId, replacedTabId) {
-	logWarn("Tab " + replacedTabId + " replaced with " + addedTabId);
-	chrome.tabs.get(addedTabId, function(tab) {
-		return initializeTab(tab, false);
-	});
+    logWarn("Tab " + replacedTabId + " replaced with " + addedTabId);
+    chrome.tabs.get(addedTabId, function(tab) {
+        return initializeTab(tab, false);
+    });
 };
 
 // Listen to all tab events to recognize user navigation to a supported website
 var addTabListeners = function() {
-	if (!chrome.tabs.onUpdated.hasListener(onTabUpdate)) {
-		chrome.tabs.onUpdated.addListener(onTabUpdate);
-	}
-	if (!chrome.tabs.onReplaced.hasListener(onTabReplace)) {
-		chrome.tabs.onReplaced.addListener(onTabReplace);
-	}
+    if (!chrome.tabs.onUpdated.hasListener(onTabUpdate)) {
+        chrome.tabs.onUpdated.addListener(onTabUpdate);
+    }
+    if (!chrome.tabs.onReplaced.hasListener(onTabReplace)) {
+        chrome.tabs.onReplaced.addListener(onTabReplace);
+    }
 };
 
 // Retroactively initialize all existing tabs (e.g. when extension is first installed)
 var retroactivelyInitExistingTabs = function() {
-	chrome.windows.getAll({
-		populate: true
-	}, function(windows) {
+    chrome.windows.getAll(
+        {
+            populate: true
+        },
+        function(windows) {
+            // For all windows
+            for (var i = 0; i < windows.length; i++) {
+                var currentWindow = windows[i];
 
-		// For all windows
-		for (var i = 0; i < windows.length; i++) {
-			var currentWindow = windows[i];
+                // For all tabs in that window
+                for (var j = 0; j < currentWindow.tabs.length; j++) {
+                    var currentTab = currentWindow.tabs[j];
 
-			// For all tabs in that window
-			for (var j = 0; j < currentWindow.tabs.length; j++) {
-				var currentTab = currentWindow.tabs[j];
-
-				// Only initialize http or https pages, not ftp://, chrome:// etc.
-				if (currentTab.url.match(/(http|https):\/\//gi)) {
-					initializeTab(currentTab, true);
-				}
-			}
-		}
-	});
+                    // Only initialize http or https pages, not ftp://, chrome:// etc.
+                    if (currentTab.url.match(/(http|https):\/\//gi)) {
+                        initializeTab(currentTab, true);
+                    }
+                }
+            }
+        }
+    );
 };
-
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------
 //  Initialization
@@ -113,21 +117,21 @@ var retroactivelyInitExistingTabs = function() {
 
 // Initialize the background script
 var initializeBackgroundScript = function() {
-	// Load all assets (JS/HTML/CSS) from disk to cache
-	loadAllAssetsToCache(true, function() {
-		// Load extension to all future tabs
-		addTabListeners();
-		// Load extension to all existing tabs
-		retroactivelyInitExistingTabs();
-	});
+    // Load all assets (JS/HTML/CSS) from disk to cache
+    loadAllAssetsToCache(true, function() {
+        // Load extension to all future tabs
+        addTabListeners();
+        // Load extension to all existing tabs
+        retroactivelyInitExistingTabs();
+    });
 
-	setInterval(function() {
-		// In dev/staging mode, reload all CSS/HTML files every second, to allow easy live development
-		// without the need to reload the extension on every change
-		if (ENVIRONMENT != 'production') {
-			loadAllAssetsToCache(false, function() {});
-		}
-	}, 1000)
+    setInterval(function() {
+        // In dev/staging mode, reload all CSS/HTML files every second, to allow easy live development
+        // without the need to reload the extension on every change
+        if (ENVIRONMENT != "production") {
+            loadAllAssetsToCache(false, function() {});
+        }
+    }, 1000);
 };
 
 initializeBackgroundScript();
--- js/content.js
+++ js/content.js
@@ -1,29 +1,120 @@
-if (typeof(EXT_NAME_CONTENT_SCRIPT_LOADED) == 'undefined') {
-	var EXT_NAME_CONTENT_SCRIPT_LOADED = true;
+if (typeof EXT_NAME_CONTENT_SCRIPT_LOADED == "undefined") {
+    var EXT_NAME_CONTENT_SCRIPT_LOADED = true;
 
-	var ExtName = {};
+    var extension = {};
 
-	//---------------------------------------------------------------------------------------------------------------------
-	ExtName.initialize = function() {
-		console.log("Initializing extension content script");
+    //---------------------------------------------------------------------------------------------------------------------
+    extension.initialize = function() {
+        console.log("Initializing extension content script");
 
-		// On document ready
-		$(document).ready(function() {
+        // On document ready
+        $(document).ready(function() {
+            // Load CSS
+            var path = chrome.extension.getURL("css/content.css");
+            var link = document.createElement("link");
+            link.setAttribute("rel", "stylesheet");
+            link.setAttribute("type", "text/css");
+            link.setAttribute("href", path);
+            document.getElementsByTagName("head")[0].appendChild(link);
+        }); // End of document.ready
 
-			// Load CSS
-			var path = chrome.extension.getURL('css/content.css');
-			var link = document.createElement('link')
-			link.setAttribute('rel', 'stylesheet')
-			link.setAttribute('type', 'text/css')
-			link.setAttribute('href', path)
-			document.getElementsByTagName('head')[0].appendChild(link)
+        // End of initialize
+    };
 
-		}); // End of document.ready
+    //---------------------------------------------------------------------------------------------------------------------
+    // Start the extension content script
+    extension.initialize();
 
-		// End of initialize
-	}
+    async function fetchAsync(url) {
+        let response = await fetch(url);
+        let data = await response.json();
+        return data;
+    }
 
-	//---------------------------------------------------------------------------------------------------------------------
-	// Start the extension content script
-	ExtName.initialize();
+    let getShowLinks = function() {
+        return [...document.getElementsByTagName("a")].filter(x => x.id.includes("title"));
+    };
+
+    let getTitles = function() {
+        const shows = getShowLinks().map(x => x.text);
+        return shows;
+    };
+
+    const APIKEY = "b365e774";
+
+    let getImdbRating = async function(title) {
+        let imdbRating = "";
+
+        let query =
+            "http://www.omdbapi.com/?s=" + title.replace(" ", "+") + "&" + "apikey=" + APIKEY;
+
+        let result = await fetchAsync(query);
+
+        if (!result["Search"]) {
+            return "";
+        }
+
+        let imdbID = result["Search"][0]["imdbID"];
+
+        query = "http://www.omdbapi.com/?i=" + imdbID + "&" + "apikey=" + APIKEY;
+        result = await fetchAsync(query);
+
+        if (result["Response"] === "True") {
+            imdbRating = result["imdbRating"];
+        }
+
+        return imdbRating;
+    };
+
+    let addFontAwesome = function() {
+        var s = document.createElement("script");
+        s.type = "text/javascript";
+        s.src = "https://kit.fontawesome.com/d66392e131.js";
+        s.crossOrigin = "anonymous";
+        document.head.appendChild(s);
+    };
+
+    let createRatingDiv = function(imdbRating) {
+        imdbRating.style = "position:relative;right:-80%;top:-2.5em";
+
+        const imdbIcon = document.createElement("i");
+        imdbIcon.className = "fab fa-imdb";
+        imdbIcon.style = "margin:0.2em;position:relative;right:-80%;top:-2.5em;";
+
+        const ratingDiv = document.createElement("div");
+        ratingDiv.className = "rating-div";
+
+        ratingDiv.appendChild(imdbIcon);
+        ratingDiv.appendChild(imdbRating);
+
+        return ratingDiv;
+    };
+
+    let addCSS = function() {
+        var s = document.createElement("style");
+        s.type = "text/javascript";
+        s.src = "https://kit.fontawesome.com/d66392e131.js";
+        s.crossOrigin = "anonymous";
+        document.head.appendChild(s);
+    };
+
+    let execute = async function() {
+        addFontAwesome();
+        const titles = getTitles();
+
+        for (let i = 0; i < titles.length; ++i) {
+            try {
+                const rating = await getImdbRating(titles[i]);
+                const imdbRating = document.createElement("span");
+                imdbRating.innerHTML = rating;
+                const ratingDiv = createRatingDiv(imdbRating);
+                getShowLinks()[i].parentElement.appendChild(ratingDiv);
+                return;
+            } catch (exception) {
+                console.error(exception);
+            }
+        }
+    };
+
+    window.onload = execute;
 }
